f9edb682b0d2c00f818bf04934232f0dd3237fd9
Chukwubuikem Chiabuotu <chibykes99@gmail.com>
2022-12-01 02:45:15 +0100

    Kadi Go Complete

---
+++ pages/_document.js
@@ -0,0 +1,22 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+	render() {
+		return (
+			<Html>
+				<Head>
+					<link rel="manifest" href="/manifest.json" />
+					<link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+					<link rel="theme-color" href="#0000" />
+					<link rel="icon" href="/img/favicon.png" />
+				</Head>
+				<body className="">
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
+}
+
+export default MyDocument;
